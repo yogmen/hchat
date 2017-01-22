f78067bf47f250e1d9795b04161834d094851023
Robert Szala <rmszala@gmail.com>
2017-01-22 21:07:57 +0100

    Initial commit

---
+++ app/src/main/java/com/szala/hchat/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.szala.hchat.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.szala.hchat.R;
+
+/**
+ * Created by robert on 22/01/2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_acitivity_layout);
+    }
+}
